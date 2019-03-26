fc3de5dcdecf71a136ab03c7c8baf90ed04baa95
Dhanush4444 <dhanushdazz@gmail.com>
2019-03-26 22:52:34 +0530

    Added base64 to hexPayload

--- Python-Codes/downLink.py
+++ Python-Codes/downLink.py
@@ -4,6 +4,8 @@ import ttn
 app_id = ""
 access_key = ""
 
+hexPayload = 'AA'.decode('hex').encode('base64')
+
 def downlinkCallback(mid,obj):
     print(obj)
 
@@ -18,6 +20,6 @@ handler = ttn.HandlerClient(app_id, access_key)
 mqtt_client = handler.data()
 mqtt_client.connect()
 mqtt_client.set_connect_callback(connect_callback)
-mqtt_client.send(dev_id="sensor1",  pay= "AA==", port=1, conf=False, sched="replace")
+mqtt_client.send(dev_id="sensor1",  pay= hexPayload, port=1, conf=False, sched="replace")
 
 mqtt_client.close()
