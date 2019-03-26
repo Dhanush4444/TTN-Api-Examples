3e321f3033445efdffd9b4beff98fb823929fea7
Dhanush4444 <dhanushdazz@gmail.com>
2019-03-26 22:16:24 +0530

    Added Uplink code

--- Python-Codes/uplinkTest.py
+++ Python-Codes/uplinkTest.py
@@ -4,6 +4,10 @@ import ttn
 app_id = "m"
 access_key = ""
 
+def uplink_callback(msg, client):
+  print("Received uplink from ", msg.dev_id)
+  print(msg)
+
 def connect_callback(res,client):
     if res == True:
         print('Connection Successfull')
@@ -15,6 +19,5 @@ handler = ttn.HandlerClient(app_id, access_key)
 mqtt_client = handler.data()
 mqtt_client.connect()
 mqtt_client.set_connect_callback(connect_callback)
-mqtt_client.set_uplink_callback(uplink_callback)
 
 mqtt_client.close()
